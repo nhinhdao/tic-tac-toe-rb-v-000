818cd9fb42cc9b71680c01c611de786c312bfa64
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-15 21:51:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -93,7 +93,11 @@ def play(board)
             turn(board)
             if won?(board)
                         puts "Congratulations #{winner(board)}!"
+            elsif draw?(board)
+                  puts "Cat's Game!"
+            else
+                  turn(board)
             end
       end
-      puts "Cat's Game!"
+
 end
